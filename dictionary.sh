d97341835840662fe261570e8691a5c69c4e7384
Shadman Afzal <shadman.afzal.7@gmail.com>
2024-11-26 21:06:30 +0530

    Update dictionary.sh
    Handle cases when API response is empty and if the meaning of  word is not found.

--- dictionary.sh
+++ dictionary.sh
@@ -2,34 +2,45 @@
 
 if [ $# -eq 0 ]; then
     echo -e "\033[1;31mError\033[0m: No arguments provided."
-    echo -e "\033[1;31mUsage\033[0m: $0 \033[1m<arg1>\033[0m"
+    echo -e "\033[1;31mUsage\033[0m: $0 \033[1m<word>\033[0m"
     exit 1
 fi
 
 if [ ! "$(command -v jq)" ]; then
-  echo -e "\033[1;31mError\033[0m: jq is not installed."
-  exit 1
+    echo -e "\033[1;31mError\033[0m: jq is not installed."
+    exit 1
 fi
 
-response=`curl -s https://api.dictionaryapi.dev/api/v2/entries/en/$1`
+response=$(curl -s -w "%{http_code}" "https://api.dictionaryapi.dev/api/v2/entries/en/$1")
+status_code="${response: -3}"
+body="${response:: -3}"
 
-meanings=$(echo "$response" | jq -r '.[0].meanings')
+if [ "$status_code" -ne 200 ]; then
+    echo -e "\033[1;31mError\033[0m: We couldn't find definitions for the word."
+    exit 1
+fi
+
+meanings=$(echo "$body" | jq -r '.[0].meanings // []')
 
 len=$(echo "$meanings" | jq length)
 
-for ((i=0; i<len; i++)); do 
+if [ "$len" -eq 0 ]; then
+    echo -e "\033[1;31mError\033[0m: No meanings found for the word."
+    exit 1
+fi
 
+for ((i=0; i<len; i++)); do
     meaning=$(echo "$meanings" | jq ".[$i]")
 
     _jq() {
-        echo "$meaning" | jq -r ${1}
+        echo "$meaning" | jq -r "$1"
     }
 
     partOfSpeech=$(_jq '.partOfSpeech')
-    definition=$(_jq '.definitions[0].definition')
-    
+    definition=$(_jq '.definitions[0].definition // "No definition available."')
+
     echo -e "\033[1;31mPart of Speech\033[0m: ${partOfSpeech^}"
-    echo -e "\033[1;31mmDefinition\033[0m: $definition"
+    echo -e "\033[1;31mDefinition\033[0m: $definition"
 
     if ((i < len - 1)); then
         echo
